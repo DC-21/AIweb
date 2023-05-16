9d3099849e2673149c54add28536c73684cdf813
DC-21 <cholahkuboko@gmail.com>
2023-05-16 17:55:37 +0200

    commit message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Navbar = () => {
+    let Links =[
+        {name:"Home",link:"/"},
+        {name:"Blog",link:"/"},
+        {name:"Services",link:"/"},
+        {name:"About",link:"/"},
+    ];
   return (
     <div className="shadow-md w-full fixed top-0 left-0">
       <div className="md:flex bg-white py-4">
@@ -8,6 +14,15 @@ const Navbar = () => {
             <span className="text-2xl text-black-600 mr-1 pt-2 "><ion-icon name="play-circle-outline"></ion-icon></span>
         DiCE
         </div>
+        <ul>
+            {
+                Links.map((link)=>(
+                    <li>
+                        <a href={link.link} className="text-gray-900 text-lg font-semibold px-4 py-2 hover:bg-gray-200 rounded-md">{link.name}</a>
+                    </li>
+                ))
+            }
+        </ul>
       </div>
     </div>
   )
