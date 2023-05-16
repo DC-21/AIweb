0144fb8e5b7d1b32b629f3c14934ddf1dc480f47
DC-21 <cholahkuboko@gmail.com>
2023-05-16 17:43:58 +0200

    commit message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="shadow-md w-full fixed top-0 left-0">
       <div className="md:flex bg-white py-4">
         <div className="font-bold text-2xl cursor-pointer flex items-center text-gray-900">
-            <span className="text-2xl text-purple-600"><ion-icon name="diamond-outline"></ion-icon></span>
+            <span className="text-2xl text-black-600 mr-1 pt-2"><ion-icon name="diamond-outline"></ion-icon></span>
         DiCE
         </div>
       </div>
