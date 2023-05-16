262c5c2b476f115bb8e679cc88c9eb5a34471e14
DC-21 <cholahkuboko@gmail.com>
2023-05-16 18:10:25 +0200

    commit message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from './Button'
 
 const Navbar = () => {
     let Links =[
@@ -10,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="shadow-md w-full fixed top-0 left-0">
       <div className="md:flex items-center justify-center bg-white py-4">
-        <div className="font-bold text-2xl cursor-pointer flex items-center text-indigo-900 container bg-gray-200 py-2 p-4">
+        <div className="font-bold text-2xl cursor-pointer flex items-center text-indigo-900 container bg--200 py-2 p-4">
             <span className="text-2xl text-black-600 mr-1 pt-2 "><ion-icon name="play-circle-outline"></ion-icon></span>
         DiCE
         </div>
@@ -18,10 +19,11 @@ const Navbar = () => {
             {
                 Links.map((link)=>(
                     <li key={link.name} className="md:ml-8 text-xl">
-                        <a href={link.link} className="text-blue-900 text-lg font-semibold px-4 py-2 hover:bg-blue-200 rounded-md">{link.name}</a>
+                        <a href={link.link} className="text-blue-900 text-lg font-semibold px-4 py-2 hover:bg-blue-200 duration-500 rounded-md">{link.name}</a>
                     </li>
                 ))
             }
+            <Button/>
         </ul>
       </div>
     </div>
