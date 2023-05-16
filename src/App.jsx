049060c23940e17abb2540c30cf4b4ffb907fdef
DC-21 <cholahkuboko@gmail.com>
2023-05-16 17:32:44 +0200

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+     <Navbar/>
+     <div className="bg-purple-400 w-full h-screen"></div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex bg-white py-4">
+        <div className="font-bold text-2xl"><span><ion-icon name="diamond-outline"></ion-icon></span>
+        DiCE
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
