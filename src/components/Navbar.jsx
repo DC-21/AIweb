68a561dd1cad5da6843365e0667a1bfd8ca48ec7
DC-21 <cholahkuboko@gmail.com>
2023-05-16 18:00:46 +0200

    commit message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,16 +9,16 @@ const Navbar = () => {
     ];
   return (
     <div className="shadow-md w-full fixed top-0 left-0">
-      <div className="md:flex bg-white py-4">
-        <div className="font-bold text-2xl cursor-pointer flex items-center text-gray-900">
+      <div className="md:flex items-center justify-center bg-white py-4">
+        <div className="font-bold text-2xl cursor-pointer flex items-center text-indigo-900">
             <span className="text-2xl text-black-600 mr-1 pt-2 "><ion-icon name="play-circle-outline"></ion-icon></span>
         DiCE
         </div>
         <ul className="md:flex md:items-center">
             {
                 Links.map((link)=>(
-                    <li>
-                        <a href={link.link} className="text-gray-900 text-lg font-semibold px-4 py-2 hover:bg-gray-200 rounded-md">{link.name}</a>
+                    <li key={link.name} className="md:ml-8">
+                        <a href={link.link} className="text-blue-900 text-lg font-semibold px-4 py-2 hover:bg-gray-200 rounded-md">{link.name}</a>
                     </li>
                 ))
             }
