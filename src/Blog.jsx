14d5d51fb064faa6e138c5540aa485c1f7ab403f
DC-21 <cholahkuboko@gmail.com>
2023-05-17 18:47:03 +0200

    commit message

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -11,8 +11,8 @@ function Blog() {
     <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
-      <h1 class="sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900">Master The Future Of AI</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
+      <h1 class="sm:text-3xl text-2xl font-bold title-font mb-4 text-black">Master The Future Of AI</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed font-bold text-black text-xl">Mastering the future of AI requires adaptability, innovation, and ethical considerations for a truly transformation impact.</p>
     </div>
     <div class="flex flex-wrap -m-4">
       <div class="lg:w-1/3 sm:w-1/2 p-4">
