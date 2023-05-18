63183f1061a497c70cf4cf61d856658e54ef2641
DC-21 <cholahkuboko@gmail.com>
2023-05-18 15:06:26 +0200

    commit message

--- src/Cta.jsx
+++ src/Cta.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Cta = () => {
   return (
-    <section className="text-gray-400 bg-white body-font">
+    <section className="text-gray-400 bg-white body-font h-screen">
       <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
-        <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-          <h1 className="title-font font-medium text-3xl text-white">Slow-carb next level shoindxgoitch ethical authentic, poko scenester</h1>
-          <p className="leading-relaxed mt-4">Poke slow-carb mixtape knausgaard, typewriter street art gentrify hammock starladder roathse. Craies vegan tousled etsy austin.</p>
+        <div className="lg:w-1/2 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+          <h1 className="title-font font-bold text-3xl text-blue-900">"Unlock the Power of AI"</h1>
+          <p className="text-2xl text-blue-900 leading-relaxed mt-4">Subscribe to Our Newsletter and Stay Ahead of the Intelligent Revolution</p>
         </div>
-        <div className="lg:w-2/6 md:w-1/2 bg-indigo-300 bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+        <div className="lg:w-1/3 md:w-1/2 mr-16 bg-indigo-300 bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
           <h2 className="text-blue-800 text-xl title-font mb-5 font-bold">Sign Up</h2>
           <div className="relative mb-4">
             <label htmlFor="full-name" className="leading-7 text-sm text-blue-600 font-bold">Full Name</label>
