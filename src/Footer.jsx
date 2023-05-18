66ba2ba3c96cf18e380345bbc59f315a058d0d06
DC-21 <cholahkuboko@gmail.com>
2023-05-18 13:51:43 +0200

    commit message

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap md:text-left text-center order-first">
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+            <h2 className="title-font font-medium text-blue-900 tracking-widest text-xl mb-3">Bots & VA</h2>
             <nav className="list-none mb-10">
               <li>
                 <a className="text-gray-400 hover:text-white">First Link</a>
@@ -23,7 +23,7 @@ const Footer = () => {
             </nav>
           </div>
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+            <h2 className="title-font font-medium text-blue-900 tracking-widest text-xl mb-3">CATEGORIES</h2>
             <nav className="list-none mb-10">
               <li>
                 <a className="text-gray-400 hover:text-white">First Link</a>
@@ -40,7 +40,7 @@ const Footer = () => {
             </nav>
           </div>
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+            <h2 className="title-font font-medium text-blue-900 tracking-widest text-xl mb-3">CATEGORIES</h2>
             <nav className="list-none mb-10">
               <li>
                 <a className="text-gray-400 hover:text-white">First Link</a>
@@ -57,16 +57,14 @@ const Footer = () => {
             </nav>
           </div>
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">SUBSCRIBE</h2>
+            <h2 className="title-font font-bold text-blue-800  tracking-widest text-sm mb-3">SUBSCRIBE</h2>
             <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
               <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
-                <label htmlFor="footer-field" className="leading-7 text-sm text-gray-400">Placeholder</label>
                 <input type="text" id="footer-field" name="footer-field" placeholder='email' className="w-full bg-white-800 rounded border bg-opacity-10 border-white focus:bg-transparent focus:ring-2 focus:ring-white focus:border-white text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
               </div>
               <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Subscribe</button>
             </div>
-            <p className="text-gray-500 text-sm mt-2 md:text-left text-center">Bitters chicharrones fanny pack
-              <br className="lg:block hidden" />waistcoat green juice
+            <p className="text-blue-800 text-sm mt-2 md:text-left text-center">Subscribe for daily updates
             </p>
           </div>
         </div>
