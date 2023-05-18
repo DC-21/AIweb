b7a24d386a87a34521f3f767b63b5524f7a01324
DC-21 <cholahkuboko@gmail.com>
2023-05-18 09:03:51 +0200

    commit message

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,28 +5,29 @@ import { Player } from '@lottiefiles/react-lottie-player';
 function Hero() {
   return (
     <section className="text-gray-600 body-font">
-        <div className="container cover mx-auto flex px-5 py-24 items-center justify-center flex-col">
-          <div className="text-center lg:w-2/3 w-full">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
+      <div className="container cover mx-auto flex px-5 py-24 items-center justify-center flex-col">
+        <div className="text-center lg:w-2/3 w-full">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
             Unleashing the Power of AI
-            </h1>
-            <Player
-        autoplay
-        loop
-        src="https://assets1.lottiefiles.com/packages/lf20_w51pcehl.json" // Replace with the path to your Lottie animation JSON file
-        style={{ width: '600px', height: '300px' }} // Set the desired width and height
-        className=' -z-10 hover:-translate-y-4 transition-all ease-in-out duration-300'
-         />
-            <p className="mb-8 leading-relaxed font-bold text-black text-2xl ">
+          </h1>
+          <Player
+            autoplay
+            loop
+            src="https://assets1.lottiefiles.com/packages/lf20_w51pcehl.json"
+            style={{ width: '600px', height: '300px' }}
+            className="-z-10 hover:-translate-y-4 transition-all ease-in-out duration-300"
+          />
+          <p className="mb-8 leading-relaxed font-bold text-black text-2xl">
             "Transforming customer experience with AI-powered chatbots and virtual assistants for seamless interactions and efficient support."
-            </p>
-            <div className="flex justify-center">
-              <Button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Get Started
-              </Button>
-            </div>
+          </p>
+          <div className="flex justify-center">
+            <Button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Get Started
+            </Button>
           </div>
+          <hr className="mt-8 mb-4 h-1 w-full border-b-2 border-indigo-500 w-1/4 mx-auto" />
         </div>
+      </div>
     </section>
   );
 }
