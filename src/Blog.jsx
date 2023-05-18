ebdab97b1f6f526a26b065999b3ba92fcba7cb7d
DC-21 <cholahkuboko@gmail.com>
2023-05-18 12:10:21 +0200

    commit message

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -12,22 +12,22 @@ function Blog() {
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -m-4">
           <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <img alt="gallery" className="w-full h-full object-cover object-center" src={ai} />
+            <img alt="gallery" className="w-full h-full object-cover object-center rounded" src={ai} />
           </div>
           <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <img alt="gallery" className="w-full h-full object-cover object-center" src={girl} />
+            <img alt="gallery" className="w-full h-full object-cover object-center rounded" src={girl} />
           </div>
           <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <img alt="gallery" className="w-full h-full object-cover object-center" src={hom} />
+            <img alt="gallery" className="w-full h-full object-cover object-center rounded" src={hom} />
           </div>
           <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <img alt="gallery" className="w-full h-full object-cover object-center" src={make} />
+            <img alt="gallery" className="w-full h-full object-cover object-center rounded" src={make} />
           </div>
           <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <img alt="gallery" className="w-full h-full object-cover object-center" src={art} />
+            <img alt="gallery" className="w-full h-full object-cover object-center rounded" src={art} />
           </div>
           <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <img alt="gallery" className="w-full h-full object-cover object-center" src={mac} />
+            <img alt="gallery" className="w-full h-full object-cover object-center rounded" src={mac} />
           </div>
         </div>
       </div>
