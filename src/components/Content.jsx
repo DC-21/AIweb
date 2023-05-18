8e07e451a3076153b94ec0167f6cb692274d1d1a
DC-21 <cholahkuboko@gmail.com>
2023-05-18 09:50:13 +0200

    commit message

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,41 +7,36 @@ const Content = () => {
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-wrap w-full mb-20">
       <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
-        <h1 className="sm:text-3xl text-2xl font-bold title-font mb-2 text-black">Our Products</h1>
+        <h1 className="sm:text-3xl text-2xl font-bold title-font mb-2 text-black justify-center">Our Products</h1>
         <div className="h-1 w-20 bg-indigo-500 rounded"></div>
       </div>
-      <p className="lg:w-1/2 w-full leading-relaxed text-gray-400 text-opacity-90">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
     </div>
     <div className="flex flex-wrap -m-4">
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content"/>
-          <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-white font-medium title-font mb-4">Chichen Itza</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <h2 className="text-lg text-white font-medium title-font mb-4">Chat Bots</h2>
+          <p className="leading-relaxed text-base">"Our Efficient chatbots streamline customer interactions."</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content"/>
-          <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-white font-medium title-font mb-4">Colosseum Roma</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <h2 className="text-lg text-white font-medium title-font mb-4">Virtual Assistants</h2>
+          <p className="leading-relaxed text-base">"Need someone to talk to, <br/> Our Virtual assistants enhance productivity seamlessly."</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content"/>
-          <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-white font-medium title-font mb-4">Great Pyramid of Giza</h2>
+          <h2 className="text-lg text-white font-medium title-font mb-4">Image Processing</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
           <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content"/>
-          <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-white font-medium title-font mb-4">San Francisco</h2>
+          <h2 className="text-lg text-white font-medium title-font mb-4">Search Engines</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
       </div>
