a1e6f0844e18b28af3210d215c108ba34a5e1ece
DC-21 <cholahkuboko@gmail.com>
2023-05-18 11:59:49 +0200

    commit message

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -8,7 +8,7 @@ import art from './images/art.jpg';
 
 function Blog() {
   return (
-    <section className="text-gray-600 body-font">
+    <section className="text-gray-600 body-font cursor-pointer">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -m-4">
           <div className="lg:w-1/3 sm:w-1/2 p-4">
