11879f4f9442709bfe916928f3a0eb1537373b35
DC-21 <cholahkuboko@gmail.com>
2023-05-18 12:40:08 +0200

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Blog from './Blog'
 import Content from './Content'
+import Cta from './Cta'
 
 function App() {
 
@@ -12,11 +13,14 @@ function App() {
      <div className="bg-indigo-200 w-full h-screen rounded">
       <Hero/>
      </div>
-     <div className="bg-indigo-200 h-full w-full">
+     <div className="bg-white h-full w-full">
       <Blog/>
      </div>
      <div>
       <Content/>
+      </div>
+      <div>
+        <Cta/>
       </div>
     </>
   )
