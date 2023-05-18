6894619293b68604351aef14ef9315ef8d2d0283
DC-21 <cholahkuboko@gmail.com>
2023-05-18 13:26:48 +0200

    commit message

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -61,9 +61,9 @@ const Footer = () => {
             <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
               <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
                 <label htmlFor="footer-field" className="leading-7 text-sm text-gray-400">Placeholder</label>
-                <input type="text" id="footer-field" name="footer-field" placeholder='email' className="w-full bg-white-800 rounded border bg-opacity-10 border-white focus:bg-transparent focus:ring-2 focus:ring-indigo-900 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                <input type="text" id="footer-field" name="footer-field" placeholder='email' className="w-full bg-white-800 rounded border bg-opacity-10 border-white focus:bg-transparent focus:ring-2 focus:ring-white focus:border-white text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
               </div>
-              <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+              <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Subscribe</button>
             </div>
             <p className="text-gray-500 text-sm mt-2 md:text-left text-center">Bitters chicharrones fanny pack
               <br className="lg:block hidden" />waistcoat green juice
@@ -71,16 +71,16 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="bg-gray-800 bg-opacity-75">
+      <div className="bg-white bg-opacity-75">
         <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span className="ml-3 text-xl">Tailblocks</span>
+            <span className="ml-3 text-xl text-black">DiCE</span>
           </a>
-          <p className="text-sm text-gray-400 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
-            <a href="https://twitter.com/knyttneve" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@knyttneve</a>
+          <p className="text-sm text-black sm:ml-6 sm:mt-0 mt-4">© 2020 DiCE —
+            <a href="https://twitter.com/knyttneve" className="text-black ml-1" target="_blank" rel="noopener noreferrer">@dcwrld</a>
           </p>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
             <a className="text-gray-400">
@@ -88,7 +88,7 @@ const Footer = () => {
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-400">
+            <a className="ml-3 text-indigo-700">
               <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
               </svg>
