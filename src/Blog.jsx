e49fffedb1acf6106cb337ec46a52e33d2a6441a
DC-21 <cholahkuboko@gmail.com>
2023-05-18 08:55:24 +0200

    commit message

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,84 +1,38 @@
-import React from 'react'
-import ai from './images/aii.png'
-import girl from './images/girl.png'
-import make from './images/gpt.png'
-import hom from './images/aii.jpg'
-import mac from './images/mac.png'
-import art from './images/art.jpg'
+import React from 'react';
+import ai from './images/aii.png';
+import girl from './images/girl.png';
+import make from './images/gpt.png';
+import hom from './images/aii.jpg';
+import mac from './images/mac.png';
+import art from './images/art.jpg';
 
 function Blog() {
   return (
-    <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-20">
-      <h1 class="sm:text-3xl text-2xl font-bold title-font mb-4 text-black">Master The Future Of AI</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed font-bold text-black text-xl">Mastering the future of AI requires adaptability, innovation, and ethical considerations for a truly transformation impact.</p>
-    </div>
-    <div class="flex flex-wrap -m-4">
-      <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src={ai}/>
-          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <img alt="gallery" className="w-full h-full object-cover object-center" src={ai} />
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <img alt="gallery" className="w-full h-full object-cover object-center" src={girl} />
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <img alt="gallery" className="w-full h-full object-cover object-center" src={hom} />
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <img alt="gallery" className="w-full h-full object-cover object-center" src={make} />
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <img alt="gallery" className="w-full h-full object-cover object-center" src={art} />
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <img alt="gallery" className="w-full h-full object-cover object-center" src={mac} />
           </div>
         </div>
       </div>
-      <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src={girl}/>
-          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src={hom}/>
-          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src={make}/>
-          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Neptune</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src={art}/>
-          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Holden Caulfield</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src={mac}/>
-          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Alper Kamu</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-  )
+    </section>
+  );
 }
 
-export default Blog
+export default Blog;
