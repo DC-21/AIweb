e3ad28d9328273c53b4f93caaf21b5a4458506c1
DC-21 <cholahkuboko@gmail.com>
2023-05-18 08:50:44 +0200

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import Hepa from './components/Hepa'
+import Hero from './components/Hero'
 import Blog from './Blog'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <>
      <Navbar/>
      <div className="bg-indigo-200 w-full h-screen">
-      <Hepa/>
+      <Hero/>
      </div>
      <div className="bg-indigo-200 h-full w-full">
       <Blog/>
