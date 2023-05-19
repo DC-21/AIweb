42b0f819a2ab55786e9231b72cbb72d287cb636b
DC-21 <cholahkuboko@gmail.com>
2023-05-19 17:39:00 +0200

    commit message

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './images/logo.png'
 
 const Footer = () => {
   return (
@@ -72,9 +73,9 @@ const Footer = () => {
       <div className="bg-white bg-opacity-75">
         <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
+          <a href="#" className="flex-shrink-0">
+              <img src={logo} alt="Logo" className=" h-16 md:h-20 w-16 md:w-20 object-contain hover:animate-pulse"/>
+            </a>
             <span className="ml-3 text-xl text-black">DiCE</span>
           </a>
           <p className="text-sm text-black sm:ml-6 sm:mt-0 mt-4">© 2020 DiCE —
