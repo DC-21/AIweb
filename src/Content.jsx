2d0090bcdd6f15e7de4dda560d20c08843a9b24a
DC-21 <cholahkuboko@gmail.com>
2023-05-19 17:14:46 +0200

    commit message

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,14 +6,14 @@ import search from './images/search.png';
 
 const Content = () => {
   return (
-    <section className="text-gray-400 body-font bg-indigo-200 rounded">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-wrap w-full mb-20">
-          <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+    <section className="text-gray-400 body-font bg-indigo-200 rounded justify-center">
+      <div className="container px-5 py-24 mx-auto justify-center">
+        <div className="flex flex-wrap w-full mb-20 items-center justify-center">
+          <div className="lg:w-1/2 w-full mb-6 lg:mb-0 items-center justify-center">
             <h1 className="sm:text-3xl text-2xl font-bold title-font mb-2 text-black justify-center">
               Our Products
             </h1>
-            <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+            <div className="h-1 w-20 bg-indigo-500 rounded items-center justify-center"></div>
           </div>
         </div>
         <div className="flex flex-wrap -m-4">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Button from './Button';
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   let Links = [
     { name: 'Home', link: '/' },
     { name: 'Blog', link: '/' },
@@ -10,54 +9,17 @@ const Navbar = () => {
     { name: 'About', link: '/' },
   ];
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
     <div className="shadow-md w-full top-0 left-0 sticky z-50">
-      <div className="md:flex items-center justify-between bg-white py-4 rounded">
-        <div className="flex items-center">
-          <div className="font-bold text-2xl cursor-pointer flex items-center text-indigo-900 container bg--200 py-2 p-4">
-            <span className="text-2xl text-black-600 mr-1 pt-2">
-              <ion-icon name="play-circle-outline"></ion-icon>
-            </span>
-            DiCE
-          </div>
-          <div>
-            <input
-              type="text"
-              placeholder="Search"
-              className="bg-white-900 border-b-2 border-indigo-400 py-2 px-4 mr-2 text-black-400 focus:outline-none"
-            />
-          </div>
+      <div className="md:flex items-center justify-center bg-white py-4 rounded">
+        <div className="font-bold text-2xl cursor-pointer flex items-center text-indigo-900 container bg--200 py-2 p-4">
+          <span className="text-2xl text-black-600 mr-1 pt-2">
+            <ion-icon name="play-circle-outline"></ion-icon>
+          </span>
+          DiCE
         </div>
-        <div className="md:hidden">
-          <button
-            className="text-gray-900 focus:outline-none"
-            onClick={toggleMenu}
-          >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            </svg>
-          </button>
-        </div>
-        <ul
-          className={`${
-            isMenuOpen ? 'flex' : 'hidden'
-          } md:flex md:items-center ml-4`}
-        >
+        <div><input type="text" placeholder="Search" className="bg-white-900 border-b-2 border-indigo-400 py-2 px-4 mr-2 text-black-400 focus:outline-none" /></div>
+        <ul className="md:flex md:items-center ml-4">
           {Links.map((link) => (
             <li
               key={link.name}
@@ -67,6 +29,7 @@ const Navbar = () => {
                 href={link.link}
                 className="text-blue-900 text-lg font-semibold px-4 py-2 hover:bg-blue-200 duration-500 rounded-md"
               >
+                
                 {link.name}
               </a>
             </li>
