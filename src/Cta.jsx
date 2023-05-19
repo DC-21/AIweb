47de3df80d3a7ce8dce378ed2d23e9443cdb6e9e
DC-21 <cholahkuboko@gmail.com>
2023-05-19 17:47:04 +0200

    commit message

--- src/Cta.jsx
+++ src/Cta.jsx
@@ -12,11 +12,11 @@ const Cta = () => {
           <h2 className="text-blue-800 text-xl title-font mb-5 font-bold">Sign Up</h2>
           <div className="relative mb-4">
             <label htmlFor="full-name" className="leading-7 text-sm text-blue-600 font-bold">Full Name</label>
-            <input type="text" id="full-name" name="full-name" className="w-full bg-white focus:bg-transparent focus:ring-2 focus:ring-white rounded focus:border-white text-base outline-none text-black py-1 px-3 leading-8 duration-200 ease-in-out" />
+            <input type="text" id="full-name" name="full-name" className="w-full white focubg-s:ring-2 focus:ring-indigo- rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <div className="relative mb-4">
             <label htmlFor="email" className="leading-7 text-sm text-blue-600 font-bold">Email</label>
-            <input type="email" id="email" name="email" className="w-full bg-white focus:ring-2 focus:ring-indigo- rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="email" id="email" name="email" className="w-full white focubg-s:ring-2 focus:ring-indigo- rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <button className="text-white bg-indigo-800 border-0 py-2 px-8 focus:outline-none font-bold hover:bg-indigo-600 rounded text-lg">Sign Up</button>
         </div>
