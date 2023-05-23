86f15b13ba4038408fffa45adae206dff9e83e92
DC-21 <cholahkuboko@gmail.com>
2023-05-23 16:35:19 +0200

    first commit

--- src/Content.jsx
+++ src/Content.jsx
@@ -33,16 +33,16 @@ const Content = () => {
               </div>
           </div>
           <div className="xl:w-1/4 md:w-1/2 p-4 flex hover:-translate-y-10 transition-all duration-500 ease-in-out cursor-pointer">
-              <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg w-full">
+              <div className="bg-white bg-opacity-40 p-6 rounded-lg w-full">
                 <img
                   className="h-40 rounded w-full object-cover object-center mb-6"
                   src={va}
                   alt="content"
                 />
-                <h1 className="text-lg text-white font-bold title-font mb-4 py-2 px-4 border border-blue-600 rounded shadow">
+                <h1 className="text-lg  text-gray-800 font-bold title-font mb-4 py-2 px-4 ">
                   AI Virtual Assistants
                 </h1>
-                <p className="leading-relaxed text-base font-bold text-white">
+                <p className="leading-relaxed text-base   text-gray-600">
                   "Need someone to talk to, <br /> Our Virtual assistants enhance productivity seamlessly."
                 </p>
               </div>
