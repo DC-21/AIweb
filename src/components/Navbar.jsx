607bc9291eff4200ad8e49a129c7144fb4d755c8
DC-21 <cholahkuboko@gmail.com>
2023-05-23 16:12:45 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/logo.png' 
+import dice from '../images/dice.png' 
 
 const handleHome = () => {
   const home = document.getElementById('home');
