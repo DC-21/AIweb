06f8b98342f475157103b9601c4b0dac556a7245
DC-21 <cholahkuboko@gmail.com>
2023-05-23 13:49:12 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,11 +30,11 @@ function Hero() {
             style={{ maxWidth: '600px', width: '100%', height: 'auto' }}
             className="z-10 hover:-translate-y-4 transition-all ease-in-out duration-300 cursor-pointer"
           />
-          <p className="mb-8 leading-relaxed font-bold text-blue-900 text-2xl">
+          <p className="mb-8 leading-relaxed font-bold text-blue-900 text-2xl pb-2">
             Transforming customer experience with AI-powered chatbots and virtual assistants for seamless interactions and efficient support.
           </p>
           <div className="flex justify-center">
-            <Button className="inline-flex text-white bg-indigo-500 border-0 py-2 pt-4 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            <Button className="inline-flex text-white bg-indigo-500 border-0 py-2 pt-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
               Get Started
             </Button>
           </div>
